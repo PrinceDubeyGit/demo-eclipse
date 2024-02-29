e94511fa9c006916896785e0b175a0bc7d122e81
princedubeygit <dubeyprince607@gmail.com>
2024-02-29 12:28:10 +0530

    commit ecl proj

---
+++ test.eci.demo/src/main/java/com/eci/demo/test/eci/demo/App.java
@@ -0,0 +1,13 @@
+package com.eci.demo.test.eci.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ test.eci.demo/src/main/java/test/ecl/Hello.java
@@ -0,0 +1,9 @@
+package test.ecl;
+
+public class Hello {
+	
+   public static void main(String[] args) {
+	   System.out.println("hello git");
+	
+  }
+}
